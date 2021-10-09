4cb22f042635abe1215e59f6be946f80d7c1fad2
Nikita <emper137137@gmail.com>
2021-10-09 19:42:17 +1000

    post request set up, proceeding with setting up the API

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
-
 app.listen(3000, () => console.log('listening at 3000'));
-
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    res.json({
+        status: 'success',
+        geo: {
+            lat: req.body.lat,
+            lon: req.body.lon
+        }
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import $ from 'jquery';
 
 if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(position => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        console.log(lat, lon);
-        // $();
+    navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+        const lat = coords.latitude;
+        const lon = coords.longitude;
+        $('#lat').append(document.createTextNode(lat));
+        $('#lon').append(document.createTextNode(lon));
+
+        const response = await fetch('/api', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({lat, lon})
+        });
+        const status = await response.json();
+        console.log(status)
     });
 } else {
     console.log('geolocation IS NOT available');
